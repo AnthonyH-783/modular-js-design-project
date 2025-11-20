777f1b1988b62d2d05d560f565a7c794fe5f0f55
Anthony <anthonnhani@gmail.com>
2025-11-20 12:20:15 -0700

    Refactored script.js

--- script.js
+++ script.js
@@ -1,49 +1,48 @@
+let people = (function () { // IIFE that modularzes code around people
 
-let people = (function() {
-  
-  let people = ["Will", "Laura"];
-  //Caching the DOM, binding events, and rendering
-  // DOM Caching
-  let el = document.getElementById("peopleModule");
-  let button = el.querySelector("button");
-  let input = el.querySelector("input");
-  let ul = el.querySelector("ul");
-  let template = el.querySelector("#people-template").innerHTML;
-  // Event Binding
-  button.addEventListener("click", addPerson);
-  ul.addEventListener("click", deletePerson);
-  // Rendering
-  function render(){
-    ul.innerHTML = Mustache.render(template, {people: people});
-  };
+    let people = ["Will", "Sarah"]; // Initial data state
 
-  // Initial rendering
-  render();
+    // Caching the DOM
+    let el = document.getElementById("peopleModule");
+    let button = el.querySelector("button");
+    let input = el.querySelector("input");
+    let ul = el.querySelector("ul");
+    let template = el.querySelector("#people-template").innerHTML;
 
-  function addPerson(value){
-    let name = (typeof value === 'string') ? value : input.value;
-    people.push(name);
-    render();
-    input.value = "";
-  };
-  function deletePerson(event){
-    let i;
-    if(typeof event === "number"){
-      i = event;
+    // Adding Event Listeners
+    button.addEventListener("click", addPerson);
+    ul.addEventListener("click", deletePerson);
+
+    // Rendering
+    function _render(){
+        ul.innerHTML = Mustache.render(template, {people: people});
     }
-    else{
-      if(event.target.classList.contains("del")){
-          let to_remove = event.target.closest("li");
-          i = Array.from(ul.children).findIndex((node) => {
-            return node === to_remove;
-        });
-      }
+    // Initial Rendering
+    _render();
+
+    // Adding function
+    function addPerson(event){
+        let person = (typeof event === "string") ? event : input.value;
+        people.push(person);
+        _render();
+        input.value = "";   
     }
-    
-    people.splice(i, 1);
-    render();
-  
-              
-};
-return {addPerson, deletePerson};
-})();
+    // Delete function
+    function deletePerson(event){
+        let index;
+        if(typeof event === "number"){
+            index = event;
+        }
+        else{
+            let li = event.target.closest("li");
+            index = (Array.from(ul.children)).findIndex((node) => node === li);
+        
+        }
+        people.splice(index, 1);
+        _render();
+    }
+
+    return {addPerson, deletePerson};
+
+
+})(); 
